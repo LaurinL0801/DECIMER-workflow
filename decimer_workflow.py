cd7d306b129b53cf69bdc90048911f739a4cd1b9
LaurinL0801 <laurin.lederer@gmail.com>
2024-11-21 12:34:23 +0100

    removed markush filter

--- decimer_workflow.py
+++ decimer_workflow.py
@@ -147,13 +147,14 @@ def get_model(args: argparse.Namespace) -> bool:
         bool: True if you want to use the HandDrawn model.
 
     Example:
- 
+
     """
-    if args.m=="True":
+    if args.m == "True":
         HandDrawn = True
     else:
         HandDrawn = False
     return HandDrawn
+
 
 def get_list_of_files(args: argparse.Namespace) -> Tuple[List[str], str]:
     """Retrieve a list of .pdf files from the specified directory.
@@ -308,9 +309,8 @@ def find_markush(smiles: str) -> bool:
                 return True
     if next_chars and any(char not in valid_characters_after_R for char in next_chars):
         return True
-    if "X" in smiles or 'Y' in smiles or 'Z' in smiles:
+    if "X" in smiles or "Y" in smiles or "Z" in smiles:
         return True
-    
 
 
 def get_smiles_with_avg_confidence(filepath: str, HandDrawn: bool) -> None:
@@ -342,7 +342,9 @@ def get_smiles_with_avg_confidence(filepath: str, HandDrawn: bool) -> None:
         for im in os.listdir(newdirpath):
             im_path = os.path.join(newdirpath, im)
             if im_path.endswith(".png"):
-                _, smiles_with_confidence = predict_SMILES(im_path, confidence=True, hand_drawn=HandDrawn)
+                _, smiles_with_confidence = predict_SMILES(
+                    im_path, confidence=True, hand_drawn=HandDrawn
+                )
                 smiles_characters = [item[0] for item in smiles_with_confidence]
                 smiles = "".join(smiles_characters)
                 smiles_list.append(smiles)
@@ -890,8 +892,8 @@ def main():
     """
     args = create_parser()
     pdf_list, directory = get_list_of_files(args)
-    markush_path = os.path.join(directory, "markush_structures")
-    os.makedirs(markush_path, exist_ok=True)
+    #markush_path = os.path.join(directory, "markush_structures")
+   # os.makedirs(markush_path, exist_ok=True)
     conf_value = get_conf_value(args)
     model = get_model(args)
     if conf_value is None:
@@ -909,21 +911,21 @@ def main():
         create_output_directory(pdf)
         get_single_pages(pdf)
         get_segments(pdf)
-        smiles_list = get_smiles_with_avg_confidence(pdf,model)
-        val = check_markush(pdf, smiles_list, markush_path)
-        if val:
-            pass
-        else:
-            out_dict = create_output_dictionary(pdf, doi)
-            df_doi_imid_smiles_conf, out_csv_path = create_output_csv(pdf, out_dict)
-            output_df_with_errors = get_parse_error(
-                df_doi_imid_smiles_conf, out_csv_path, terminal_output
-            )
-            output_df_with_errors_and_pred_im = get_predicted_images(
-                output_df_with_errors
-            )
-            create_pdf(pdf, output_df_with_errors_and_pred_im)
-            move_pdf(pdf)
+        smiles_list = get_smiles_with_avg_confidence(pdf, model)
+        #val = check_markush(pdf, smiles_list, markush_path)
+        #if val:
+        #    pass
+        #else:
+        out_dict = create_output_dictionary(pdf, doi)
+        df_doi_imid_smiles_conf, out_csv_path = create_output_csv(pdf, out_dict)
+        output_df_with_errors = get_parse_error(
+            df_doi_imid_smiles_conf, out_csv_path, terminal_output
+        )
+        output_df_with_errors_and_pred_im = get_predicted_images(
+            output_df_with_errors
+        )
+        create_pdf(pdf, output_df_with_errors_and_pred_im)
+        move_pdf(pdf)
         current_time = extract_absolute_times()
         time_list.append(current_time)
     csv_path = concatenate_csv_files(abs_path_input, output_file="merged_output.csv")
